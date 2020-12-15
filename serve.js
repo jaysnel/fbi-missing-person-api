2567ecde8396dd71bd0bb7016f4f5aa619e84222
root <jaylan210@gmail.com>
2020-12-15 18:39:19 -0500

    learned about cors, helment and morgan. Setup example of what route should look like

--- serve.js
+++ serve.js
@@ -1,10 +1,26 @@
 //Using this tuturial to get my bearings https://medium.com/@dinyangetoh/how-to-build-simple-restful-api-with-nodejs-expressjs-and-mongodb-99348012925d
 //Data
-let express = require('express')
-let app = express();
-var port = process.env.PORT || 8080;
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
-app.get('/', (req, res) => res.send('Some Data here'));
+let app = express();
+let port = process.env.PORT || 8080;
+
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan('combined'));
+
+let exampledata = [{name: "John", age: 25}, {name: "Mary", age: 23}]
+
+app.get('/all-data', (req, res) => {
+     res.send(exampledata);
+});
+
 app.listen(port, function () {
      console.log("Running on port " + port);
 });
