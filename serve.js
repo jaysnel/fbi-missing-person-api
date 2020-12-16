c5e93a784d3feaa61e6f3851caf5d210192275e4
root <jaylan210@gmail.com>
2020-12-15 20:31:09 -0500

    connecting to DB. page with full list is complete for now

--- serve.js
+++ serve.js
@@ -1,5 +1,4 @@
-//Using this tuturial to get my bearings https://medium.com/@dinyangetoh/how-to-build-simple-restful-api-with-nodejs-expressjs-and-mongodb-99348012925d
-//Data
+//General data
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -7,6 +6,17 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
+//MongoDB data
+const mongodb = require('mongodb');
+const mongoclient = mongodb.MongoClient;
+const username = process.env.CLUSTER_USERNAME;
+const password = process.env.CLUSTER_PASSWORD;
+const databasename = process.env.DATABASE_NAME;
+const nameofcollection = process.env.COLLECTION_NAME;
+const uri = `mongodb+srv://${username}:${password}@cluster0.suitu.mongodb.net/${databasename}>?retryWrites=true&w=majority`;
+const client = new mongoclient(uri, { useNewUrlParser: true, useUnifiedTopology: true  });
+
+//Connection data
 let app = express();
 let port = process.env.PORT || 8080;
 
@@ -15,11 +25,29 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan('combined'));
 
-let exampledata = [{name: "John", age: 25}, {name: "Mary", age: 23}]
 
-app.get('/all-data', (req, res) => {
-     res.send(exampledata);
+//Return All Data
+app.get('/', (req, res) => {
+
+     let getFullList = async() => {
+     //Add final list to DB
+     client.connect(async(err) => {
+          if(err) return catchError(err);
+
+          const collection = await client.db(databasename).collection(nameofcollection);
+          
+          let data = await collection.find({"Found": false}).toArray();
+          res.send(data);
+          client.close();
+          });
+     }
+
+     getFullList();
 });
+
+
+
+
 
 app.listen(port, function () {
      console.log("Running on port " + port);
