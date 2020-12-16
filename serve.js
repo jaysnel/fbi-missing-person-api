d77d107f2e8da2edeb1e6fd8d237eb3cd3c1293d
root <jaylan210@gmail.com>
2020-12-15 20:56:38 -0500

    multiple connections, remoing _id from query

--- serve.js
+++ serve.js
@@ -14,7 +14,7 @@ const password = process.env.CLUSTER_PASSWORD;
 const databasename = process.env.DATABASE_NAME;
 const nameofcollection = process.env.COLLECTION_NAME;
 const uri = `mongodb+srv://${username}:${password}@cluster0.suitu.mongodb.net/${databasename}>?retryWrites=true&w=majority`;
-const client = new mongoclient(uri, { useNewUrlParser: true, useUnifiedTopology: true  });
+let client;
 
 //Connection data
 let app = express();
@@ -27,27 +27,35 @@ app.use(morgan('combined'));
 
 
 //Return All Data
-app.get('/', (req, res) => {
+app.get('/v1/all', (req, res) => {
 
      let getFullList = async() => {
-     //Add final list to DB
+
+     returnNewClientInstance();
      client.connect(async(err) => {
           if(err) return catchError(err);
 
           const collection = await client.db(databasename).collection(nameofcollection);
           
-          let data = await collection.find({"Found": false}).toArray();
+          let data = await collection.find({}, { projection: { _id: 0 } }).toArray();
           res.send(data);
           client.close();
           });
+     
      }
 
      getFullList();
 });
 
+//Other Functions
+//Re-initializing client, was not able to make this global as it never get reinitialized
+function returnNewClientInstance() {
+     return client = new mongoclient(uri, { useNewUrlParser: true, useUnifiedTopology: true  });
+}
 
-
-
+function catchError(err) {
+     console.log(err);
+ }
 
 app.listen(port, function () {
      console.log("Running on port " + port);
